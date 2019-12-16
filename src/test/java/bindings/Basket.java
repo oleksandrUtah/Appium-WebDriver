2f5021829b2b80989ff8f54b55df457200c6ce89
AlexUtah <ste.kozachenko@outlook.com>
2019-12-16 15:08:15 -0700

    12/16/2019

--- src/test/java/bindings/Basket.java
+++ src/test/java/bindings/Basket.java
@@ -2,17 +2,20 @@ package bindings;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Action;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import java.util.concurrent.TimeUnit;
+
+import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
 public class Basket {
     WebDriver driver;
+    String quantity = "20";
+    String actualQuantity;
 
     @Given("^that i am on the shopping website$")
     public void that_i_am_on_the_shopping_website() throws Throwable {
@@ -20,27 +23,42 @@ public class Basket {
                 "\\src\\test\\resources\\chromedriver.exe");
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
-        driver.get("https://www.edgewordstraining.co.uk/demo-site/");
+        driver.get("https://test.sugaringfactory.com/");
         driver.manage().window().maximize();
     }
     @When("^i add an item to the basket$")
     public void i_add_an_item_to_the_basket() throws Throwable {
-        driver.findElement(By.cssSelector("#masthead [type='search']")).click();
-        driver.findElement(By.cssSelector("#masthead [type='search']")).sendKeys("cap");
-        driver.findElement(By.cssSelector("#masthead [type='search']")).sendKeys(Keys.ENTER);
-        driver.findElement(By.cssSelector(".entry-summary button")).click();
+        driver.findElement(By.xpath("//div[contains(text(),'Soft')]")).click();
+        driver.findElement(By.xpath("//input[@name='quantity']")).clear();
+        driver.findElement(By.xpath("//input[@name='quantity']")).sendKeys(quantity);
+        driver.findElement(By.xpath("//input[@id='button-cart']")).click();
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+        Thread.sleep(3000);
+        driver.findElement(By.xpath("//div[@class='success']//i[@class='icon-remove-sign']")).click();
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
     }
     @Then("^i can view the item in my basket$")
     public void i_can_view_the_item_in_my_basket() throws Throwable {
-        driver.findElement(By.linkText("Cart")).click();
-        assertTrue(driver.findElement(By.linkText("Cap")).isDisplayed());
-        driver.findElement(By.linkText("×")).click();
-        // explicit wait for item to be removed as may use AJAX
-        WebDriverWait wait = new WebDriverWait(driver, 20); //explicit wait if we want to use
-        WebElement returnToShopLink;
-        returnToShopLink = wait.until(ExpectedConditions.visibilityOfElementLocated(By.linkText("Return to shop")));
-        returnToShopLink.click();
-        //driver.findElement(By.linkText("Return to shop")).click();
+        Actions builder = new Actions(driver);
+        Action mouseOverHome  = builder
+                .moveToElement(driver.findElement(By.xpath("//div[@id='cart']")))
+                .build();
+        for(int r=0; r<10; r++) {
+            try {
+                driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+                mouseOverHome.perform();
+                driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+                Thread.sleep(3000);
+                actualQuantity = driver.findElement(By.xpath("//span[@class='quantity']")).getText();
+                System.out.println("Added to cart: " + actualQuantity);
+                assertEquals(actualQuantity,"X " + quantity);
+                break;
+            } catch(StaleElementReferenceException e) {
+                e.toString();
+                System.out.println("Trying to recover from a stale element :" + e.getMessage());
+            }
+        }
+        Thread.sleep(3000);
         driver.quit();
     }
 }
